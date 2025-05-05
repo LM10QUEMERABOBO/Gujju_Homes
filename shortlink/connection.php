4224e8e47b13421199d6a627625cc4497aecbb6a
krish_soni <krishsoni198@gmail.com>
2025-05-05 15:55:33 +0530

    Initial commit

---
+++ shortlink/connection.php
@@ -0,0 +1,11 @@
+<?php 
+//Database connection
+error_reporting(0);
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gujju_homes";
+
+//Connection query
+$conn = mysqli_connect($server, $username, $password, $dbname);
+?>
